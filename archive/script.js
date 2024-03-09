e2fb634229352335009ecb8d2494ab5008b304a0
jaqaileen <jq.pramana@gmail.com>
2024-03-08 19:51:32 -0500

    json?

--- archive/script.js
+++ archive/script.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){ 
-    $(window).scroll(function(){
-      $('.grid-item-middle').css('transform', 'translate3d(0,' + $(this).scrollTop()*2 + 'px, 0)'); 
-   }).scroll();
-});  
+$(document).scroll(function(){
+   let scrollTop = $(this).scrollTop();
+   $('.left, .right').css('transform', 'translateY('+ scrollTop * 2 +'px)');
+});
+
+$(document).scroll(function(){
+   let scrollTop = $(this).scrollTop();
+   let scrollDirection = scrollTop > lastKnownScrollY ? 'down' : 'up';
+
+   $('.left, .right').css('transform', 'translateY('+ scrollTop * 2 +'px)');
+
+   if (scrollDirection === 'down') {
+       $('.left, .right').removeClass('bounce'); // Remove bounce class when scrolling down
+   } else {
+       $('.left, .right').addClass('bounce'); // Add bounce class when scrolling up
+   }
+
+   lastKnownScrollY = scrollTop;
+});
